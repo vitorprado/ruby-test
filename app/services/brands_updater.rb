1f1d1524dced5e787aa2b136b906afaefb2019bd
Vitor Prado <jvirtuprado@gmail.com>
2016-03-22 20:23:51 -0300

    Make brands updater filter non case sensitive

--- app/services/brands_updater.rb
+++ app/services/brands_updater.rb
@@ -1,7 +1,7 @@
 class BrandsUpdater
   
   def self.update()
-    existing_brands = Brand.all.pluck :name
+    existing_brands = Brand.all.pluck(:name).map(&:downcase)
     Brand.create self.fetch_and_filter(existing_brands)
   end
 
@@ -10,7 +10,7 @@ class BrandsUpdater
   def self.fetch_and_filter(saved_brands)
     WebmotorsFetcherService
       .fetch("marcas")
-      .reject{ |item| saved_brands.include?(item["Nome"]) }
+      .reject{ |item| saved_brands.include?(item["Nome"].downcase) }
       .map{ |item| { id: item["Id"], name: item["Nome"]} }
   end
 end
